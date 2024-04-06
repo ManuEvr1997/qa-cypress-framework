581bdd157aed61958e74e1dea75852465bc6325b
ManuEvr1997 <eveg7719@gmail.com>
2024-04-05 19:45:50 -0500

    Update cart.cy.js

--- cypress/e2e/front-end/cart.cy.js
+++
@@ -1,8 +0,0 @@
-/**describe('Cart Feature', () => {
-    it('TC-001: BE | Add new product to cart ', () => {
-       cy.visit("http://www.testingyes.com/onlineshop/");
-       cy.contens('my store')
-});
-
-#js-product-list-header > div > h1
-}); **/
