a2d48a64939edf95f0b27f38281561577a309d78
ManuVeroz <86752333+ManuEvr1997@users.noreply.github.com>
2024-04-05 19:57:43 -0500

    Update example.cy.js

--- cypress/e2e/back-end/example.cy.js
+++ cypress/e2e/back-end/example.cy.js
@@ -1,24 +1,6 @@
-describe('validate differents posible test scenarios about user feature', () => {
-        it("GET - all users", () => {
-        cy.request("GET", "https://api.escuelajs.co/api/v1/users").should((response) => {
-          expect(response.status).to.eq(200);
+describe('Cart Feature', () => {
+  it('TC-001: BE | Add new product to cart ', () => {
+     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+     cy.contens('')
         });
-      });
-
-      it("POST API testing Using Cypress API Plugin", () => {
-        cy.request("POST", "https://api.escuelajs.co/api/v1/users", {
-          name: "Nicolas",
-          email: "nico@gmail.com",
-          password: "12345",
-          avatar: "https://api.lorem.space/image/face?w=640&h=480",
-        }).should((response) => {
-          expect(response.status).to.eq(201);
-        });
-     });
-     it("PUT - API testing Using Flip Plugin", () => {
-        cy.request("PUT", "https://api.escuelajs.co/api/v1/users/104", {name: "Sandra"}).should((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('name','Sandra')//true
-      });
-    });
-});
+}); 
