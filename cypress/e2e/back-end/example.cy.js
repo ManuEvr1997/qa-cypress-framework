a9ce63574d14d71ae98826d7adacf15d601dbbdc
ManuVeroz <86752333+ManuEvr1997@users.noreply.github.com>
2024-04-05 20:03:37 -0500

    Update example.cy.js

--- cypress/e2e/back-end/example.cy.js
+++ cypress/e2e/back-end/example.cy.js
@@ -1,6 +1,10 @@
-describe('Cart Feature', () => {
-  it('TC-001: BE | Add new product to cart ', () => {
-     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-     cy.contens('')
+describe('Entries feature', () => {
+    it("TC-003: GET all entries", () => {
+        cy.request("GET", "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login").then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.not.be.null;
+            cy.validateSchema(getAllEntriesSchema, response.body);
+            cy.log(JSON.stringify(response.body));
         });
-}); 
+    });
+});
